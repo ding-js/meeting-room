f84f9137b0601d2c8f1fbf0f34dcf8710b31d727
Ding Yuchao <dingyuchao@patsnap.com>
2018-06-07 13:07:48 +0800

    update command

--- run.bat
+++ run.bat
@@ -1,7 +1,8 @@
 set DATA_DIR=C:/data/meeting-room
+call docker-compose down
 cd client
 call yarn || npm i
 call npm run build
 cd ../
-call docker-compose down
-docker-compose up
+call docker-compose build
+docker-compose up -d
